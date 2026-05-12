ca54d9ab236b0ded754d2861155780928c9afbb3
Natania Samuel <nataniasamuel14@gmail.com>
2026-05-12 12:19:46 +0000

    Completed core star schema: Joined orders with users and order items for revenue analysis.

--- models/fct_orders.sql
+++ models/fct_orders.sql
@@ -7,13 +7,20 @@ order_items AS (
         order_id, 
         sale_price 
     FROM `bigquery-public-data.thelook_ecommerce.order_items`
+),
+
+users AS (
+    SELECT * FROM {{ ref('dim_users') }}
 )
 
 SELECT 
     orders.order_id,
     orders.user_id,
+    users.gender,
+    users.country,
     orders.status,
     SUM(order_items.sale_price) as total_revenue
 FROM orders
 LEFT JOIN order_items ON orders.order_id = order_items.order_id
-GROUP BY 1, 2, 3
+LEFT JOIN users ON orders.user_id = users.user_id
+GROUP BY 1, 2, 3, 4, 5
